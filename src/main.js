3bb14c78d406ef1bec285f29f3a28b3ac430f055
Tayrone <jose.tayrone@ctis.com.br>
2020-07-30 13:42:27 -0300

    Promises, async and await

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
-import UsuarioRenomeado from './function'
-import { idade } from './function'
-import Usuario, { idade as IdadeUsuario } from "./function";
+// CRIANDO PROMISE MANUAL
 
-console.log(UsuarioRenomeado.info())
-console.log(idade);
-console.log(IdadeUsuario);
+const minhaPromise = () => new Promise((resolve, reject) => {
+	setTimeout(() => { resolve('OK')}, 2000);
+});
+
+// UTILIZANDO O ASYNC AWAIT
+// ADICIONADO O ASYNC AUTOMATICAMENTE A FUNÇÃO VIRA UMA PROMISE
+// O AWAIT SO PODE SER UTILIZADO NUMA FUNÇÃO COM ASYNC
+async function executaPromise() {
+	console.log(await minhaPromise());
+	// SEGUNDA LINHA SO EXECUTA DEPOIS DA PRIMEIRA
+	console.log(await minhaPromise());
+	console.log(await minhaPromise());
+	console.log(response)
+
+	// UTILIZANDO SINTAXE COM .THEN:
+	// myPromise().then((response) => {
+	// 	console.log(response);
+	// 	myPromise().then((response) => {
+	// 		console.log(response);
+	// 		myPromise().then((response) => {
+	// 			console.log(response);
+	// 		})
+	// 	})
+	// })
+}
+
+executaPromise();
+
+// ASYNC AWAIT COM ARROW function
+const executaPromiseArrow = async () => {
+	console.log(await myPromise());
+	console.log(await myPromise());
+	console.log(await myPromise());
+}
+
+executaPromiseArrow();
