e1dde1ff18b8f9ddcf8801bf88f596c218936510
Tayrone <jose.tayrone@ctis.com.br>
2020-07-30 09:28:10 -0300

    Import and exports ways

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import { soma } from './funcoes'
+import soma, { div } from './funcoes'
+import { sub as subtracao } from './funcoes'
+import * as funcoes from './funcoes';
 
 console.log(soma(1, 3));
+console.log(subtracao(1, 3));
+console.log(div(1, 3));
+console.log(funcoes.soma(1, 3));
